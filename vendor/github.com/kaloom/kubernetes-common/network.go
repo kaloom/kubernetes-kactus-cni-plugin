3153b3d1069d323770d1c70a1a8cdb5929622367
Tom Lebreux <tole@kaloom.com>
2022-11-01 10:50:00 -0400

    Update kubernetes-common to latest

--- vendor/github.com/kaloom/kubernetes-common/network.go
+++ vendor/github.com/kaloom/kubernetes-common/network.go
@@ -30,11 +30,12 @@ const (
 // NetworkConfig is a struct of the Pod's annotations networks element
 // (a json array of NetworkConfig)
 type NetworkConfig struct {
-	NetworkName string   `json:"name"`                  // required parameter: the network name for the CRD network resource in k8s
-	IfMAC       string   `json:"ifMac,omitempty"`       // optional parameter: the network device mac address in the form of 00:11:22:33:44:55
-	IsPrimary   bool     `json:"isPrimary,omitempty"`   // optional parameter: specify that this network is associated with the primary device in the Pod i.e. eth0
-	UpperLayers []string `json:"upperLayers,omitempty"` // optional parameter: specify the upper layers for this network i.e. a hierarchy in which this network is a lower layer of another network(s)
-	Namespace   string   `json:"namespace,omitempty"`   // optional parameter: the namespace to which this network belongs to, if not specified it would be the namespace of the pod
+	NetworkName  string   `json:"name"`                   // required parameter: the network name for the CRD network resource in k8s
+	IfMAC        string   `json:"ifMac,omitempty"`        // optional parameter: the network device mac address in the form of 00:11:22:33:44:55
+	IsPrimary    bool     `json:"isPrimary,omitempty"`    // optional parameter: specify that this network is associated with the primary device in the Pod i.e. eth0
+	UpperLayers  []string `json:"upperLayers,omitempty"`  // optional parameter: specify the upper layers for this network i.e. a hierarchy in which this network is a lower layer of another network(s)
+	Namespace    string   `json:"namespace,omitempty"`    // optional parameter: the namespace to which this network belongs to, if not specified it would be the namespace of the pod
+	PodagentSkip bool     `json:"podagentSkip,omitempty"` // optional parameter: if true, then podagent won't try to configure this network
 }
 
 // GetNetworkIfname from a networkName return a device name by
